2fff606bc5b27d9bd24cecbd245f48ba7318df60
Stephen <158498287+0-Blu@users.noreply.github.com>
2024-10-14 23:34:50 -0400

    Getting ready for the AppStore.

--- StikTools/Views/HomeView.swift
+++ StikTools/Views/HomeView.swift
@@ -25,8 +25,6 @@ struct HomeView: View {
         AppTool(imageName: "flashlight.off.circle", title: "Flashlight", color: Color.green.opacity(0.2), destination: AnyView(FlashlightView())),
     ]
     @State private var searchText: String = ""
-    @State private var newsItems: [NewsItem] = [] // News items will be loaded here
-    @State private var showPrivacyPolicyAlert: Bool = false // State for showing the privacy policy alert
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
@@ -57,26 +55,6 @@ struct HomeView: View {
                         }
                         .padding(.horizontal)
                         .padding(.top, geometry.safeAreaInsets.top + 10)
-                        
-                        // News Section
-                        if !newsItems.isEmpty {
-                            VStack(alignment: .leading) {
-                                Text("Latest News")
-                                    .font(.headline)
-                                    .foregroundColor(.primaryText)
-                                    .padding(.horizontal)
-                                ScrollView(.horizontal, showsIndicators: false) {
-                                    HStack(spacing: 10) { // Reduced spacing from 15 to 10
-                                        ForEach(newsItems) { item in
-                                            NewsCard(newsItem: item)
-                                                .frame(width: geometry.size.width * (geometry.size.width > 600 ? 0.4 : 0.8))
-                                        }
-                                    }
-                                    .padding(.horizontal)
-                                }
-                            }
-                            .padding(.bottom, 10)
-                        }
                         
                         // Join Discord Button Styled as an App Tool
                         FancyCard {
@@ -132,94 +110,15 @@ struct HomeView: View {
             }
         }
         .navigationViewStyle(StackNavigationViewStyle())
-        .onAppear {
-            #if targetEnvironment(simulator)
-            loadLocalNews() // Load local news for simulator
-            #else
-            loadNews() // Load news from the server on a real device
-            #endif
-            
-            // Show privacy policy alert if not accepted
-            if (!hasAcceptedPrivacyPolicy) {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    showPrivacyPolicyAlert = true
-                }
-            }
-        }
         .onReceive(timer) { _ in
             refreshBackground()
-        }
-        .alert(isPresented: $showPrivacyPolicyAlert) {
-            Alert(
-                title: Text("Privacy Policy"),
-                message: Text("By using this app, you agree to our privacy policy. Please review it carefully."),
-                primaryButton: .default(Text("Read Policy"), action: {
-                    if let url = URL(string: "https://stiktools.xyz/privacy.html") {
-                        UIApplication.shared.open(url)
-                    }
-                }),
-                secondaryButton: .default(Text("Accept"), action: {
-                    hasAcceptedPrivacyPolicy = true
-                })
-            )
         }
     }
 
     // Function to open Discord link
     func joinDiscord() {
-        if let url = URL(string: "https://discord.gg/qwcxgjg7rc") {
+        if let url = URL(string: "https://discord.gg/a6qxs97Gun") {
             UIApplication.shared.open(url)
-        }
-    }
-
-    // Function to load news from JSON
-    func loadNews() {
-        guard let url = URL(string: "https://stiktools.xyz/news-2.json") else { return }
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let data = data {
-                do {
-                    let decodedNews = try JSONDecoder().decode([NewsItem].self, from: data)
-                    DispatchQueue.main.async {
-                        newsItems = decodedNews
-                    }
-                } catch {
-                    print("Failed to decode news JSON: \(error)")
-                }
-            }
-        }.resume()
-    }
-
-    // Function to load local news (for simulator)
-    func loadLocalNews() {
-        // Attempt to find the path of the local JSON file
-        if let path = Bundle.main.path(forResource: "localNews", ofType: "json") {
-            do {
-                // Attempt to load the contents of the file into a Data object
-                let data = try Data(contentsOf: URL(fileURLWithPath: path))
-                
-                // Optional: Print the raw JSON data as a string for debugging
-                if let jsonString = String(data: data, encoding: .utf8) {
-                    print("JSON Data: \(jsonString)")
-                }
-                
-                // Attempt to decode the JSON data into an array of NewsItem objects
-                let decodedNews = try JSONDecoder().decode([NewsItem].self, from: data)
-                
-                // Update the newsItems array with the decoded data
-                newsItems = decodedNews
-            } catch let DecodingError.dataCorrupted(context) {
-                print("Data corrupted: \(context.debugDescription)")
-            } catch let DecodingError.keyNotFound(key, context) {
-                print("Key '\(key)' not found: \(context.debugDescription), codingPath: \(context.codingPath)")
-            } catch let DecodingError.typeMismatch(type, context) {
-                print("Type '\(type)' mismatch: \(context.debugDescription), codingPath: \(context.codingPath)")
-            } catch let DecodingError.valueNotFound(value, context) {
-                print("Value '\(value)' not found: \(context.debugDescription), codingPath: \(context.codingPath)")
-            } catch {
-                print("Failed to load local news JSON: \(error.localizedDescription)")
-            }
-        } else {
-            print("Failed to find the JSON file.")
         }
     }
 
